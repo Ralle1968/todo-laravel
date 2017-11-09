051542ec85955fca82c9d43746f36ed8d4095776
Ralle1968 <graef.ralf@gmail.com>
2017-11-09 23:26:12 +0100

    change view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel, yeah!
                 </div>
 
                 <div class="links">
